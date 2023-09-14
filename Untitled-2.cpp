1d885e097c7b243ea3d1baa8c43d73b26825dba3
2312Ramreddy <142864537+2312Ramreddy@users.noreply.github.com>
2023-09-14 05:38:52 -0400

    Add files via upload

--- Untitled-2.cpp
+++ Untitled-2.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-int x = 20;
-int y = 18;
-if (x > y) {
-cout<<"x is greater than y";
-}
-return 0;
+int main()
+{
+    int arr[5];
+    arr[0]=5;
+    arr[2]=-10;
+    //this is same as arr[1]=2
+    arr[3/2]=2;
+    arr[3]=arr[0];
+    cout<<arr[0]<<""<<arr[1]
+    <<""<<arr[2]<<""<<arr[3];
+    return 0;
+
 }
