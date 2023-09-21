a855d0c77a6161645ea464ac10741d158ba953bb
2312Ramreddy <142864537+2312Ramreddy@users.noreply.github.com>
2023-09-21 15:22:04 +0530

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,6 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main() {
-    cout <<"Helloo world";
-return 0;
+
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    for(int i = 1; i <= rows; ++i) {
+        for(int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
 }
