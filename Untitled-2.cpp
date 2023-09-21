89715bdcce2ab70979402fe8d962ed1384d1fff8
2312Ramreddy <142864537+2312Ramreddy@users.noreply.github.com>
2023-09-21 15:22:23 +0530

    Add files via upload

--- Untitled-2.cpp
+++ Untitled-2.cpp
@@ -1,15 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int arr[5];
-    arr[0]=5;
-    arr[2]=-10;
-    //this is same as arr[1]=2
-    arr[3/2]=2;
-    arr[3]=arr[0];
-    cout<<arr[0]<<""<<arr[1]
-    <<""<<arr[2]<<""<<arr[3];
-    return 0;
 
+int main() {
+
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = rows; i >= 1; --i) {
+        for(int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
 }
